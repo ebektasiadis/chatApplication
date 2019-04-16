7607707d81c8e6ec9ae15c8e1284cab47eca5683
Efstratios Bektasiadis <mpektastratos1997@gmail.com>
2019-04-17 02:17:46 +0300

    Added notes
    kudos: @anastasiosmx

--- client.c
+++ client.c
@@ -10,6 +10,7 @@
 #include "clientFunctions.h"
 
 int socketv;
+//Ο πίνακας που κάνουμε map τις εντολές
 router_t indexes[] = {
   {NEW_USER, sizeof(C_newUser), sizeof(S_newUser)},
   {GET_TOTAL_USERS, 0, sizeof(S_getTotalUsers)},
@@ -18,9 +19,10 @@ router_t indexes[] = {
   {GET_MESSAGES, 0, sizeof(S_getMessages)},
 };
 
-void sigHandler(int sigID){
-  sendCmd(DISCONNECT, NULL);
-  exit(1);
+//Συνάρτηση που κάνει handle το CTRL+C που πατάει ο client
+void sigHandler(int sigID){ //Εάν πατηθεί CTRL+C μπες εδω
+  sendCmd(DISCONNECT, NULL);  //Στείλε στον server την εντολή DISCONNECT
+  exit(1);  //Τερμάτισε
 }
 
 
@@ -29,20 +31,22 @@ int main(int argc, char* argv[]){
   struct sockaddr_in socketAddr;
   C_newUser cNewUser;
 
-  signal(SIGINT, sigHandler);
-  signal(SIGTSTP, sigHandler);
+  signal(SIGINT, sigHandler); // Πλέον το σήμα SIGINT γίνεται handle σύμφωνα με την συνάρτηση sigHandler
+  signal(SIGTSTP, sigHandler);  // Πλέον το σήμα SIGTSTP γίνεται handle σύμφωνα με την συνάρτηση sigHandler
 
-  socketv = socket(AF_INET, SOCK_STREAM, 0);
+  socketv = socket(AF_INET, SOCK_STREAM, 0);  //Φτιάξε το socket
 
   if(socket<0){
       perror("socket");
       exit(1);
   }
 
+  //Ρυθμίσεις για το socket
   socketAddr.sin_family = AF_INET;
   socketAddr.sin_port = htons(atoi(argv[1]));
   socketAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
+  //Κάνε connect στο server με βάση τις παραπάνω ρυθμίσεις
   connectv = connect(socketv, (struct sockaddr *)&socketAddr, sizeof(socketAddr));
 
   if(connectv<0){
@@ -50,70 +54,69 @@ int main(int argc, char* argv[]){
       exit(1);
   }
 
-  cNewUser = usernameInput();
+  cNewUser = usernameInput(); //Δες ClientFunctions.h
 
-  char msg[MAX_MESSAGE_LENGTH], msgToBeTokenized[MAX_MESSAGE_LENGTH];;
-  char* word;
-  char** words = (char**)malloc(sizeof(char*));
-  int totalWords;
+  char msg[MAX_MESSAGE_LENGTH], msgToBeTokenized[MAX_MESSAGE_LENGTH]; //Δύο πίνακες ο ένας έχει το μήνυμα του χρήστη και το άλλο είναι αντιγραφή αυτόυ για να κάνουμε την strtok (γιατί η strtok καταστρέφει το string)
+  char* word; //Αποθηκεύω κάθε φορά τη λέξη που βγαίνει απο την strtok
+  char** words = (char**)malloc(sizeof(char*)); //Δεσμέυω χώρο για τον πίνακα words που βάζω μέσα τις λέξεις που γίνονται tokenized
+  int totalWords; //Counter
 
   printf("Connected. Type /? or /help to view the available commands.\n");
 
   while(true){
-    fgets(msg, sizeof(msg), stdin);
-    msg[strlen(msg)-1] = '\0';
-    totalWords = 0;
-    system("clear");
+    fgets(msg, sizeof(msg), stdin); //Όμοια λειτουργία με scanf, παίρνω είσοδο απο το χρήστη.
+    msg[strlen(msg)-1] = '\0';  //Βάλε στο τέλος '\0' για να τερματίσει το string
+    totalWords = 0; //Αρχικοποίηση counter
+    system("clear");  //Εντολή για να καθαρίσει το output της οθόνης
 
-    strcpy(msgToBeTokenized, msg);
-    word = strtok(msgToBeTokenized, " ");
+    strcpy(msgToBeTokenized, msg);  //Αντιγραφή του μηνύματος απο τον πίνακα msg στον πίνακα msgToBeTokenized για το λόγο που περιγράψαμε παραπάνω.
+    word = strtok(msgToBeTokenized, " "); //Ξεκίνα την strtok, με delimeter το κενό. Δηλαδη πάρε το string, και κάθε φορά που βρίσκεις κενό
 
-    while(word != NULL) {
-      totalWords++;
-      words = (char**)realloc(words, sizeof(char*)*totalWords);
-      words[totalWords-1] = (char*)malloc(sizeof(char) * strlen(word));
-      strcpy(words[totalWords-1], word);
-      word = strtok(NULL, " ");
+    while(word != NULL) { //Για όσο βρίσκεις λέξεις
+      totalWords++; //Αύξησε τον counter
+      words = (char**)realloc(words, sizeof(char*)*totalWords); //Μεγάλωσε τον πίνακα κατα 1 γραμμή για να μπει η λέξη με realloc
+      words[totalWords-1] = (char*)malloc(sizeof(char) * strlen(word)); //Μεγάλωσε τον πίνακα όσες στήλες είναι το μέγεθος της λέξης με realloc
+      strcpy(words[totalWords-1], word);  //Βάλε τη λέξη στο πίνακα που μόλις μεγάλωσες.
+      word = strtok(NULL, " "); //Ξανά strtok την επόμενη λέξη
     }
 
-    if(totalWords == 0)
+    if(totalWords == 0) //Εάν δεν βρεις λέξεις απλά κάνε continue
       continue;
 
     if(!strcmp(words[0], "/u") || !strcmp(words[0], "/users")){
-      showUsers();
+      showUsers();  //Δες ClientFunctions.h
     }else if(!strcmp(words[0], "/r") || !strcmp(words[0], "/refresh")){
-      showMessages();
+      showMessages(); //Δες ClientFunctions.h
     }else if(!strcmp(words[0], "/e") || !strcmp(words[0], "/exit")){
-      sendCmd(DISCONNECT, NULL);
+      sendCmd(DISCONNECT, NULL);  //Επικοινωνία με server
       exit(0);
     }else if(!strcmp(words[0], "/?")  || !strcmp(words[0], "/help")){
       printf("Chat Application\n");
       printf("/u OR /users   :: Show online users\n");
       printf("/r OR /refresh :: Refresh messages\n");
       printf("/e OR /exit    :: Exit application (Can be happen with CTRL+C too)\n");
-    }else if(strlen(words[0]) > 0 && words[0][0] == '@' && totalWords > 1){
-      sendCmd(POST_MESSAGE, msg);
-      showMessages();
+    }else if(strlen(words[0]) > 0 && words[0][0] == '@' && totalWords > 1){ //Εάν θέλει να στείλει μύνημα 'έχει μπροστά @'τότε
+      sendCmd(POST_MESSAGE, msg); //Στείλε το μύνημα στο server με κωδικό POST_MESSAGE
+      showMessages(); //Δες ClientFunctions.h
     }else{
-      printf("Commmand not found\n");
+      printf("Commmand not found\n"); //Αν γράψει οτιδήποτε άλλο, απλά πες του ότι δεν υπάρχει αυτό το commmand.
     }
-    //FREE ARRAY
   }
 
   return 0;
 }
 
-void* sendCmd(int cmd, void* data){
+void* sendCmd(int cmd, void* data){ //Συνάρτηση που μιλάω με το server
   void* response;
 
-  write(socketv, &cmd, sizeof(cmd));
-  if(cmd >= 0){
-    response = (void*)malloc(indexes[cmd].lServer);
-    if(indexes[cmd].lClient > 0){
-      write(socketv, data, indexes[cmd].lClient);
+  write(socketv, &cmd, sizeof(cmd));  //Γράψε στο server το είδος της εντολής που να περιμένει (ώστε αν ξερει πόσα bytes να κανει read)
+  if(cmd >= 0){ //Εάν είναι να του στείλεις κάποια εντολή με cmd_code >= 0
+    response = (void*)malloc(indexes[cmd].lServer); //Δέσμευσε χώρο στη μεταβλητή response, τόσο όσο ορίζεται απο το είδος της εντολής cmd και περιμένουμε απο το server
+    if(indexes[cmd].lClient > 0){ //Εάν είσαι σε φάση που θες να στείλεις κάτι, και για αυτό το μέγεθος του client > 0
+      write(socketv, data, indexes[cmd].lClient); //Γράψε στο server αυτό που θες να του στείλεις
     }
-    if(indexes[cmd].lServer > 0){
-      read(socketv, response, indexes[cmd].lServer);
+    if(indexes[cmd].lServer > 0){ //Εάν είσαι σε φάση που θες να λάβεις κάτι, και για αυτό το μέγεθος του server > 0
+      read(socketv, response, indexes[cmd].lServer);  //Διάβασε απο το server αυτο που θέλει να σου στείλει
     }
   }
 
--- server.c
+++ server.c
@@ -9,66 +9,68 @@
 #include "structs.h"
 #include "serverFunctions.h"
 
-router_t indexes[] = {
-  {NEW_USER, sizeof(C_newUser), sizeof(S_newUser), newUser},
-  {GET_TOTAL_USERS, 0, sizeof(S_getTotalUsers), getTotalUsers},
-  {GET_USER, sizeof(C_getUser), sizeof(S_getUser), getUser},
-  {POST_MESSAGE, sizeof(C_postMessage), 0, postMessage},
-  {GET_MESSAGES, 0, sizeof(S_getMessages), getMessages},
+router_t indexes[] = {  //Δημιουργώ το struct που ουσιαστικά είναι ο πίνακας με το mapping των commands
+  {NEW_USER, sizeof(C_newUser), sizeof(S_newUser), newUser},  //Δημιουργία νέου User
+  {GET_TOTAL_USERS, 0, sizeof(S_getTotalUsers), getTotalUsers}, //Επιστροφή όλων των online users
+  {GET_USER, sizeof(C_getUser), sizeof(S_getUser), getUser},  // Επιστροφή πληροφοριών για τον χρήστη με το index που βρίσκεται στο Client struct.
+  {POST_MESSAGE, sizeof(C_postMessage), 0, postMessage},  //Ετοιμασία του μυνήματος για αποστολή
+  {GET_MESSAGES, 0, sizeof(S_getMessages), getMessages},  //Ετοιμασία για παραλαβή μυνήματος.
 };
 
-void disconnect(threadArgs_t threadArgs){
+void disconnect(threadArgs_t threadArgs){ //Ουσιαστικά αφαιρεί τον χρήστη που έφυγε απο τη λίστα με τους online χρήστες.
   int i, target;
   char tmp[MAX_USERNAME_LENGTH], tmp2[MAX_USERNAME_LENGTH];
 
-  if(strlen(threadArgs.username) == 0)
+  if(strlen(threadArgs.username) == 0)  //Αν έφυγε πριν βάλει καν username μην κάνεις τίποτα αφού δεν πρόλαβε να μπει στη λίστα με τους online ετσι και αλλιως.
     return;
 
   for(i = 0; i < *threadArgs.totalConnected; i++){
     if(!strcmp(threadArgs.username, (*threadArgs.usernames)[i])){
-      target = i;
+      target = i; //Τρέξε το πίνακα με τους online χρηστες και βρες αυτον που έφυγε. Αποθήκευσε τη θέση του.
       break;
     }
   }
 
+  //Κλασσικός τρόπος swap μεταξύ του χρήστη που έφυγε με τον τελευταίο στο πίνακα.
   if(*threadArgs.totalConnected-1 > 0){
     strcpy(tmp, (*threadArgs.usernames)[*threadArgs.totalConnected-1]);
     strcpy(tmp2, (*threadArgs.usernames)[target]);
     strcpy((*threadArgs.usernames)[*threadArgs.totalConnected-1], tmp2);
     strcpy((*threadArgs.usernames)[target], tmp);
   }
+  //Αφου παω τον χρήστη που έφυγε στο τέλος του πίνακα κάτω, κάνω realloc τον πίνακα και τον μικραίνω κατα ένα. Επομένως ο τελευταιος χάνεται απο τον πίνακα με τους online.
   *threadArgs.usernames = (char**)realloc(*threadArgs.usernames, sizeof(char*) * --(*threadArgs.totalConnected));
 }
 
-void* core(void* args){
-  threadArgs_t threadArgs = *(threadArgs_t*)args;
+void* core(void* args){ //Συνάρτηση που διαχειρίζεται το thread του client. Παίρνει σαν είσοδο το struct με τα στοιχεία του χρήστη που δημιουργήσαμε
+  threadArgs_t threadArgs = *(threadArgs_t*)args; //type-casting του void* struct που πέρασα ως είσοδο, προκειμένου αν μπορώ αν το επεξεργαστώ μέσα στη συνάρτηση
 
   while(true){
     int cmd;
     void* input;
-    data_t data;
+    data_t data;  //Δημιουργώ το struct για τα δεδομένα που θα διαχειριστεί και θα στείλει ο server
 
-    read(threadArgs.sID, &cmd, sizeof(int));
-    printf("Received cmd: %d\n", cmd);
-    if(cmd >= 0){
-      printf("For this command you will receive %zu bytes and send back %zu bytes.\n", indexes[cmd].lClient, indexes[cmd].lServer);
-      input = (void*)malloc(indexes[cmd].lClient);
-      if(indexes[cmd].lClient > 0){
-        printf("Read from client %lu bytes\n", read(threadArgs.sID, input, indexes[cmd].lClient));
-        data.dClient = input;
+    read(threadArgs.sID, &cmd, sizeof(int));  //Περιμένω να διαβάσω απο τον χρήστη (στην αρχή περιμένω το username)
+    printf("Received cmd: %d\n", cmd);  //DEBUG print
+    if(cmd >= 0){ //Εάν πάρω απο το χρήστη οποιαδήποτε εντολή με cmd code > 0 (δες πίνακα με defines.h) κάνε τα παρακάτω
+      printf("For this command you will receive %zu bytes and send back %zu bytes.\n", indexes[cmd].lClient, indexes[cmd].lServer); //DEBUG print πόσα mb να περιμένω και πόσα πρέπει να στείλω
+      input = (void*)malloc(indexes[cmd].lClient);  //Κάνω type cast απο void* και δεσμεύσω χώρο, τόσο όσο είναι τα bytes που περιμένω απο το χρήστη στη μεταβλητή input
+      if(indexes[cmd].lClient > 0){ //Εάν περιμένω να μου στείλει κάτι ο χρήστης, τότε μπες και διάβασε αυτά που θέλει να σου στείλει
+        printf("Read from client %lu bytes\n", read(threadArgs.sID, input, indexes[cmd].lClient));  //Διαβάζω τι έχει να μου στείλει ο χρήστης.
+        data.dClient = input; //Αποθήκευση των "bytes" που μου έστειλε ο χρήστης στο data.dClient
       }
 
-      data.threadArgs = &threadArgs;
-      data.dServer = (void*)malloc(indexes[cmd].lServer);
-      indexes[cmd].function(&data);
+      data.threadArgs = &threadArgs;  //Αποθηκεύω τα στοιχεία του χρήστη στο data.threadArgs
+      data.dServer = (void*)malloc(indexes[cmd].lServer); //Δεσμέυω χώρο για αυτά που θα στείλω τόσο όσο ξέρω ότι θα χρειαστεί να στείλω.
+      indexes[cmd].function(&data); //Του λέω πήγενε στην συνάρτηση που σου ορίζει ο πίνακας των commands (βάση της εντολής), και του περνάω σαν είσοδο το "data" που περιέχει όλα τα στοιχεία που πρέπει να πάνε για επεξεργασία. Σαν να τα έχω πακετάρει δηλαδή και να στελνω όλα μαζί μέσα στο κουτί "data"
 
-      if(indexes[cmd].lServer > 0){
-        printf("Wrote on client %lu bytes\n", write(threadArgs.sID, data.dServer, indexes[cmd].lServer));
+      if(indexes[cmd].lServer > 0){ //Εαν έχει να στείλει κάτι ο server μπες μεσα στην if και κάνε write όπου είναι να κάνεις write τα επεξεργασμένα πλέον δεδομένα.
+        printf("Wrote on client %lu bytes\n", write(threadArgs.sID, data.dServer, indexes[cmd].lServer)); //Στέλνω τα δεδομένα απο το server σε client.
       }
 
     }else{
       switch(cmd){
-        case DISCONNECT: disconnect(threadArgs); pthread_exit(NULL); break;
+        case DISCONNECT: disconnect(threadArgs); pthread_exit(NULL); break; //Σε περίπτωση που ο χρήστης στείλει μήνυμα εξόδου, κάνε τον disconect (αφαίρεσε τον απο τη λίστα των online) και κλείσε το thread του.
       }
     }
   }
@@ -77,51 +79,53 @@ void* core(void* args){
 int main(int argc, char* argv[]){
   int totalConnected = 0;
 
-  int bindv, socketv, clientv, threadv;
-  struct sockaddr_in socketAddr;
+  int bindv, socketv, clientv, threadv; //Μεταβλητές για τα sockets και τα thread
+  struct sockaddr_in socketAddr;  //Struct για τα sockets
 
-  threadArgs_t threadArgs;
+  threadArgs_t threadArgs;  //Δημιουργώ μια μεταβλητη τύπου threadArgs_t την οποία θα γεμίσω στη συνέχεια με τα στοιχεία του νέου χρήστη
 
-  socketv = socket(AF_INET, SOCK_STREAM, 0);
+  socketv = socket(AF_INET, SOCK_STREAM, 0);  //Δημιουργία Socket
 
   if(socket<0){
     perror("socket");
     exit(1);
   }
 
+  //Ρυθμίσεις για το socket
   socketAddr.sin_family = AF_INET;
   socketAddr.sin_port = htons(atoi(argv[1]));
   socketAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  bindv = bind(socketv, (struct sockaddr *)&socketAddr, sizeof(socketAddr));
+  bindv = bind(socketv, (struct sockaddr *)&socketAddr, sizeof(socketAddr));  //Ένωη όλων των παραπάνω ρυθμίσεων του socket
 
   if(bindv<0){
     perror("bind");
     exit(1);
   }
 
-  if(listen(socketv, 1) < 0){
+  if(listen(socketv, 1) < 0){ //Περιμένω για εισερχόμενες συνδέσεις.
     perror("listen");
     exit(1);
   }
 
-  char** usernames = (char**)malloc(sizeof(char*));
-  messages_t messages;
-  messages.totalMessages = 0;
-  messages.messagesIndex = (messageIndex_t*)malloc(sizeof(messageIndex_t));
+  char** usernames = (char**)malloc(sizeof(char*)); //Δημιουργία και δέσμευση μνήμης για το σισδιάστατο πίνακα usernames
+  messages_t messages;  //Δημιουργία struct τύπου messages_t
+  messages.totalMessages = 0; //Αρχικοποίηση μυνημάτων σε 0
+  messages.messagesIndex = (messageIndex_t*)malloc(sizeof(messageIndex_t)); //Δέσμευση χώρου για το messagesIndex. Pending...
 
   //Threading.
   while(true){
-    clientv = accept(socketv, NULL , NULL);
+    clientv = accept(socketv, NULL , NULL); //Δέχομαι ένα νέο client
     if(clientv < 1){
       perror("accept");
     }
 
-    printf("New user logged in and found username pointer at %p\n", &usernames);
+    printf("New user logged in and found username pointer at %p\n", &usernames);  //DEBUG print
 
-    if(pthread_create(&threadArgs.tID, NULL, core, &threadArgs) != 0){
-      perror("pthread_create");
+    if(pthread_create(&threadArgs.tID, NULL, core, &threadArgs) != 0){  //Δημιουργία του thread για το νέο client με την εντολή "pthread_create"
+      perror("pthread_create");                                         //Λέω στο πρόγραμμα μου ότι το thread αυτό θα το διαχειρίζεται η συνάρτηση "core"
     }else{
+      //Γεμίζω το struct threadArgs_t με τα στοιχεία του νέου χρήστη που μόλις συνδέθηκε
       threadArgs.sID = clientv;
       threadArgs.usernames = &usernames;
       threadArgs.messages = &messages;
